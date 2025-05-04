9ba2eb3b87ce4a2d42d88cbccc852b9bbce35f08
Achmad Hendarsyah <ahmdhndrsyh@gmail.com>
2025-05-04 09:57:20 +0700

    feat: swagger support, example request

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,26 @@
 import { AllExceptionsFilter } from '@core/filters';
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.setGlobalPrefix('api');
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
+
+  // Swagger
+  const config = new DocumentBuilder()
+    .setTitle('API Docs')
+    .setDescription('The list of APIs')
+    .setVersion('1.0')
+    .addTag('Your Great APIs')
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, documentFactory, {
+    jsonDocumentUrl: 'docs/json',
+  });
 
   app.useGlobalFilters(new AllExceptionsFilter());
   await app.listen(process.env.PORT ?? 3000);
